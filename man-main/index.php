430c211885bbb60c3e0306ca31010befdd379924
gigigiy <93245129+gigigiy@users.noreply.github.com>
2021-11-16 18:09:08 +0500

    Update index.php

--- man-main/index.php
+++ man-main/index.php
@@ -12,14 +12,20 @@
     <div class="information">
         <ul>
             <li>
-                Phone: 5465465465454
+                Phone:0702109563
             </li>
             <li>
                 Adress: Sovetskaya
             </li>
             <li>
-                name: Timur 
+                name: Timur Aralbaev
             </li>
+            </li>
+        Birth Date:15.04.2005
+        </li>
+     </li>
+    Status:student
+    </li>
         </ul>
     </div>  
     <div class="photo">
